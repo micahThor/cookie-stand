aa062379ad43c896347059fdda6b611db328cb05
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-12 16:54:55 -0800

    refactored manager object

--- js/app.js
+++ js/app.js
@@ -1,104 +1,121 @@
 'use strict';
 
-function setRandomCookies(min, max, sale) {
-  return Math.ceil(Math.floor(Math.random() * (max - min + 1) + min) * sale);
+function main() {
+  // create new location manager and add locations
+  var locationMgr = new BakeryLocationManager();
+  locationMgr.addLocation(new BakeryLocation('Seattle', 23, 65, 6.3));
+  locationMgr.addLocation(new BakeryLocation('Tokyo', 3, 24, 1.2));
+  locationMgr.addLocation(new BakeryLocation('Dubai', 11, 38, 3.7));
+  locationMgr.addLocation(new BakeryLocation('Paris', 20, 38, 2.3));
+  locationMgr.addLocation(new BakeryLocation('Lima', 2, 16, 4.6));
+  // render table with data
+  locationMgr.renderSalesData();
 }
 
-var seattle = {
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgSale: 6.3,
+// BakeryLocation represents a bakery in a given city
+function BakeryLocation(name, minCustomer, maxCustomer, avgSale) {
+  this.name = name;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgSale = avgSale;
+}
 
-  getRandomCookies: function () {
-    return setRandomCookies(this.minCustomer, this.maxCustomer, this.avgSale);
+BakeryLocation.prototype.getRandomCookies = function () {
+  return Math.ceil(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgSale);;
+}
+
+// BakerLocationManager manages each BakeryLocation 
+function BakeryLocationManager() {
+  this.locationList = [];
+  this.tableElementId = 'salesDataTable';
+  this.hoursOfOperation = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM',
+    '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
+}
+
+// function adds a BakeryLocation object instance array
+BakeryLocationManager.prototype.addLocation = function (locationName) {
+  this.locationList.push(locationName);
+}
+
+// function drives the rendering of sales data to html table
+BakeryLocationManager.prototype.renderSalesData = function () {
+  // get parent container
+  var parentElement = document.getElementById(this.tableElementId);
+  // create table element
+  var tableElement = this.addElement('table', parentElement);
+  // create table header of sales hours
+  this.renderHourHeader(tableElement);
+  // create table data for each location
+  this.renderSalesDataForLocation(tableElement);
+  // create table footer for total cookies per hour
+  this.renderCookieTotalPerHour(tableElement);
+}
+
+// function creates a table header for sales data table.  Displays an hour in each cell
+BakeryLocationManager.prototype.renderHourHeader = function (parentElement) {
+  // create table row
+  var rowElement = this.addElement('tr', parentElement);
+  // create empty cell at upper right of table
+  this.addElement('th', rowElement);
+  // creates table header for every hour location is open
+  for (var hour = 0; hour < this.hoursOfOperation.length; hour++) {
+    this.addElement('th', rowElement, this.hoursOfOperation[hour]);
+  }
+  // add header for total cell
+  this.addElement('th', rowElement, 'Daily Total Location');
+}
+
+// function creates a table data for sales data table.  Displays amount of cookies sold in an hour
+BakeryLocationManager.prototype.renderSalesDataForLocation = function (parentElement) {
+  // loop variables
+  var rowElement;
+  var randomCookieAmt;
+  var cookieCounterPerLocation = 0;
+
+  // populate each row with cell data for each location
+  for (var location = 0; location < this.locationList.length; location++ , cookieCounterPerLocation = 0) {
+    // create table row for each location
+    rowElement = this.addElement('tr', parentElement);
+    this.addElement('th', rowElement, this.locationList[location].name);
+
+    // create table cell data for each hour for cookies sold
+    for (var hour = 0; hour < this.hoursOfOperation.length; hour++) {
+      randomCookieAmt = this.locationList[location].getRandomCookies();
+      this.addElement('td', rowElement, randomCookieAmt);
+      cookieCounterPerLocation += randomCookieAmt;
+    }
+
+    // create table cell for total amount of cookies for each location
+    this.addElement('td', rowElement, cookieCounterPerLocation);
   }
 }
 
-var tokyo = {
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgSale: 1.2,
-
-  getRandomCookies: function () {
-    return setRandomCookies(this.minCustomer, this.maxCustomer, this.avgSale);
-  }
-}
-
-var dubai = {
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgSale: 3.7,
-
-  getRandomCookies: function () {
-    return setRandomCookies(this.minCustomer, this.maxCustomer, this.avgSale);
-  }
-}
-
-var paris = {
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgSale: 2.3,
-
-  getRandomCookies: function () {
-    return setRandomCookies(this.minCustomer, this.maxCustomer, this.avgSale);
-  }
-}
-
-var lima = {
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgSale: 4.6,
-
-  getRandomCookies: function () {
-    return setRandomCookies(this.minCustomer, this.maxCustomer, this.avgSale);
-  }
-}
-
-
-// arrays section
-var locationObjects = [seattle, tokyo, dubai, paris, lima];
-var locationElementId = ['seattle', 'tokyo', 'dubai', 'paris', 'lima'];
-var hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM',
-  '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-
-
-// loop variables
-var parentElement;
-var ulElement;
-var liElement;
-var cookieAmountPerHour;
-
-// calculates cookie sales per hour for every location and adds to html list tags
-// renders on sales.html page
-// create html list elements for each location
-for (var i = 0, totalCookiesPerDay = 0; i < locationObjects.length; i++ , totalCookiesPerDay = 0) {
-  parentElement = document.getElementById(locationElementId[i]);
-  ulElement = document.createElement('ul');
-  parentElement.appendChild(ulElement);
-
-  // gets cookie sales for each hour and adds cookie amount to li elements
-  for (var j = 0; j < hours.length; j++) {
-    // get cookie sale and add to total
-    cookieAmountPerHour = locationObjects[i].getRandomCookies()
-    totalCookiesPerDay += cookieAmountPerHour;
-    // generate list item and add cookie data at specified time
-    liElement = document.createElement('li');
-    liElement.textContent = `${hours[j]}  : ${cookieAmountPerHour}`;
-    ulElement.appendChild(liElement);
-  }
+BakeryLocationManager.prototype.renderCookieTotalPerHour = function(parentElement) {
+  // loop variables
+  var cookieCountPerHour = 0;
   
-  // generate list item and add cookie total for a location
-  liElement = document.createElement('li');
-  liElement.textContent = `Total  : ${totalCookiesPerDay}`;
-  ulElement.appendChild(liElement);
+  //console.log(parentElement.rows[1].cells[1].innerHTML);
+  //console.log(parentElement.rows[1].cells.length);
+  //console.log(parentElement.rows.length);
+
+  var rowElement = this.addElement('tr', parentElement);
+  this.addElement('th', rowElement, 'Totals');
+  
+  for (var i = 1; i < parentElement.rows.length - 1; i++) {
+    for (var j = 1; j < parentElement.rows[i].cells.length - 1; j++) {
+      console.log(parentElement.rows[i].cells[j].innerHTML);
+    }
+  }
 
 }
 
+// Function creates a new element.  Returns newly created element
+BakeryLocationManager.prototype.addElement = function (tag, container, text) {
+  var newElement = document.createElement(tag);
+  newElement.textContent = text;
+  container.appendChild(newElement);
+  return newElement;
+}
 
-// TO DO
-// 1.  CREATE ADDELEMENT FUNCTION THAT ACCEPTS TAG, CONTAINER, TEXT
-//     FUNCTION MAKES NEW ELEMENT, ADDS TO CONTAINER, AND ADDS TEXT CONTENT
-//     FUNCTION RETURNS THE NEWLY CREATED ELEMENT (IF NEEDED)
-//
-// 2. CREATE FUNCTIONS FOR OBJECT OUTSIDE OF CONSTRUCTOR FUNCTION
-//    USE [OBJECTNAME].prototype.[FUNCTION NAME] = function() {}
+// Start program
+main();
